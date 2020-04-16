9b6d31acee8a12d56f5accf52edae895a33ace93
navsing <navdeep_singh02@outlook.com>
2020-04-16 16:30:43 -0700

    Initial Commit

---
+++ FetchAPIResponse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FetchAPIResponse
+//
+//  Created by Navdeep Singh on 4/16/20.
+//  Copyright © 2020 Navdeep Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
