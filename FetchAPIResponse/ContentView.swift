530d5db76f1794fa5fa5a482cf71012c609623d6
navsing <navdeep_singh02@outlook.com>
2020-04-16 17:43:33 -0700

    boiler plate with model, view and viewModel

--- FetchAPIResponse/ContentView.swift
+++ FetchAPIResponse/ContentView.swift
@@ -9,9 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    
+    @State var results = [TaskEntry]()
+    
+    func loadData() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/todos") else {
+            print("Your API end point is Invalid")
+            return
+        }
+        let request = URLRequest(url: url)
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let response = try? JSONDecoder().decode([TaskEntry].self, from: data) {
+                    DispatchQueue.main.async {
+                        self.results = response
+                    }
+                    return
+                }
+            }
+        }.resume()
     }
+    
+    var body: some View {
+        List(results, id: \.id) { item in
+            VStack(alignment: .leading) {
+                Text(item.title)
+            }
+        }.onAppear(perform: loadData)
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ FetchAPIResponse/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  FetchAPIResponse
+//
+//  Created by Navdeep Singh on 4/16/20.
+//  Copyright © 2020 Navdeep Singh. All rights reserved.
+//
+
+import Foundation
+
+struct TaskEntry: Codable {
+    var id: Int
+    var title: String
+}
+
